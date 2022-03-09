c07a3ecb9453af91cd261d708186a73dd09634ef
mraheeldev <m.raheeldev@gmail.com>
2022-03-09 15:26:02 +0500

    Initial commit

---
+++ app/src/main/java/com/raheel/retrofitintro/APIInterface.java
@@ -0,0 +1,30 @@
+package com.raheel.retrofitintro;
+
+import com.raheel.retrofitintro.pojo.MultipleResource;
+import com.raheel.retrofitintro.pojo.User;
+import com.raheel.retrofitintro.pojo.UserList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+
+interface APIInterface {
+
+    @GET("/api/unknown")
+    Call<MultipleResource> doGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
